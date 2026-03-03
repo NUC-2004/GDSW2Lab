46c9a6c54ea6634607268e8e73f2e21251f36b11
NUC-2004 <728697938@qq.com>
2026-03-03 23:24:24 +1100

    animation

--- Assets/Settings/Scripts/Playermovement.cs
+++ Assets/Settings/Scripts/Playermovement.cs
@@ -4,20 +4,34 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 8f;
     public float jumpForce = 12f;
+
     private Rigidbody2D rb;
+    private Animator anim;
     private bool isGrounded;
     private Vector3 spawnPoint;
+    private SpriteRenderer sr;
+
+    private float moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         spawnPoint = transform.position;
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        anim.SetBool("isWalking", moveInput != 0);
+        anim.SetBool("isGrounded", isGrounded);
+
+        if (moveInput > 0.1f)
+            sr.flipX = false;
+        else if (moveInput < -0.1f)
+            sr.flipX = true;
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
@@ -25,14 +39,16 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
         }
 
-        if (transform.position.y < -10f) 
+        if (transform.position.y < -10f)
         {
             transform.position = spawnPoint;
             rb.velocity = Vector2.zero;
         }
+    }
 
-        if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
-        else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
